f591e81c8b43a121ffaa1c7cdd6453fea612cde6
Phil Stricker <stricker.phil@gmail.com>
2015-01-23 20:44:53 -0700

    Removal of old grid...js files

--- scripts/grids.js
+++
@@ -1,7 +0,0 @@
-﻿define(["grid1.min", "grid2.min", "grid3.min", "grid4.min", "grid5.min"], function (Grid1, Grid2, Grid3, Grid4, Grid5) {
-    var grids = [
-        Grid1, Grid2, Grid3, Grid4, Grid5
-    ];
-
-    return grids;
-});
